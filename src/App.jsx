41652dc4591cc4fcfa57e9359ecaec2630616510
unknown <kevin.sm89@gmail.com>
2024-09-17 22:21:04 +0200

    side bar a medias

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <div className='app-layout'>
 
     <Sidebar/>
-    <div>asd</div>
+    <div className='main'>asd</div>
     </div>
 
     </>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,13 +2,31 @@ import { useState } from 'react'
 import './Sidebar.css'
 
 function Sidebar() {
-  const [count, setCount] = useState(0)
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  // Function to open the sidebar
+  const openSidebar = () => {
+    setSidebarOpen(((prevState) => !prevState));
+  };
+
+  // Function to close the sidebar
+  const closeSidebar = () => {
+    setSidebarOpen(false);
+  };
 
   return (
     <>
 
 <div className='lateral-menu'>
-menu lateral
+
+  <div className={`sidebar  ${isSidebarOpen ? 'visible' : 'hidden'}`} id="mySidebar">
+    <a href="#" className="w3-bar-item w3-button">Link 1</a>
+    <a href="#" className="w3-bar-item w3-button">Link 2</a>
+    <a href="#" className="w3-bar-item w3-button">Link 3</a>
+  </div>
+
+<button onClick={openSidebar} className="button-sidebar">{isSidebarOpen ? '<' : '>'}</button>
+
 </div>
     </>
   )
