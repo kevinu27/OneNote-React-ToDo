2a35ec5e1369eccfa29b6f10e64e8910ab4bced2
unknown <kevin.sm89@gmail.com>
2024-11-03 11:45:58 +0100

    side bar con los navlinks

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './Sidebar.css'
 import { useSelector, useDispatch } from 'react-redux'
 import { menuActions } from '../store/index'
+import { NavLink } from 'react-router-dom'
 
 const menuBotons = [
   {
@@ -27,9 +28,9 @@ function Sidebar() {
   };
 
   // Function to close the sidebar
-  const closeSidebar = () => {
-    setSidebarOpen(false);
-  };
+  // const closeSidebar = () => {
+  //   setSidebarOpen(false);
+  // };
 
   const clickSelectMenuHandler = (icon) => {
     console.log('ICON', icon)
@@ -38,16 +39,30 @@ function Sidebar() {
 
   return (
     <>
-
+<div className='side-bar-app'>
 <div className='lateral-menu'>
 
   <div className={`sidebar  ${isSidebarOpen ? 'visible' : 'hidden'}`} id="mySidebar">
-    <a href="#" className="menu-item" onClick={()=>clickSelectMenuHandler('todo')}>To Do</a>
-    <a href="#" className="menu-item" onClick={()=>clickSelectMenuHandler('canvas')}>One Note</a>
+    <div className='links'>
+      <NavLink
+      to='/'
+      className={({isActive}) => (isActive ? 'active' : 'inactive')}
+      onClick={()=>clickSelectMenuHandler('canvas')}
+      >One Note</NavLink>
+
+      <NavLink
+      to='/tasks'
+      className={({isActive}) => (isActive ? 'active' : 'inactive')}
+      onClick={()=>clickSelectMenuHandler('todo')}
+      >To Do</NavLink>
+    </div>
+ 
+
   </div>
 
 <div onClick={openSidebar} className="button-sidebar">{isSidebarOpen ? '<' : '>'}</div>
 
+</div>
 </div>
     </>
   )
