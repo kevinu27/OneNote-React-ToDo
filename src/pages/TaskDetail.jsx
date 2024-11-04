15ac014b5211af1ed303ce9f04234bbb1a5dcded
unknown <kevin.sm89@gmail.com>
2024-11-04 23:56:13 +0100

    styling card detail

--- src/pages/TaskDetail.jsx
+++ src/pages/TaskDetail.jsx
@@ -14,10 +14,69 @@ function TaskDetail() {
 
   return (
     <>
-    <div className="mainCardDetail">
-      
-      <p>name:{Task.name}</p>
-      <p>descripcion:{Task.description}</p>
+    <div className="taskDetail">
+      <div className="headerTaskDetail">
+        <div className="title-go-back">
+          <h2>-</h2>
+          <h2>Task Detail</h2>
+        </div>
+        <div className="edit-button">
+          <div>icon</div>
+          <div>edit task</div>
+        </div>
+      </div>
+    
+
+      <div className="mainCardDetail">
+        <div className="cardUpper">
+          <p className="taskName">{Task.name}</p>
+
+          <div className="status-buttons">
+            {/* pending, in progress, etc */}
+          </div>
+        </div>
+
+        <div class="divider-line"></div>
+        <div className="cardLower">
+        <p className="taskDescripcion">descripcion</p>
+        <p  className="taskDescripcionValue">{Task.description}</p>
+        </div>
+
+        <div className="cardDates">
+          <div className="createdDate">
+            <div className="date-icon">
+            icon
+
+            </div>
+            <div className="date-text">
+              <p>a</p>
+              <p>a</p>
+            </div>
+          </div>
+
+          <div className="createdDue">
+          <div className="date-icon">
+            icon
+            </div>
+            <div className="date-text">
+              <p>a</p>
+              <p>a</p>
+            </div>
+          </div>
+        </div>
+        <div class="divider-line2"></div>
+        <div className="bottom-card">
+          <div className="marginDiv"></div>
+          <div className="bottom-card2">
+            <div className="comment-icon">
+              icon
+            </div>
+            <textarea  type="text" required  placeholder='Añadr comentario...'></textarea> 
+          </div>
+        </div>
+
+
+      </div>
     </div>
     </>
   )
